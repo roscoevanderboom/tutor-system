d1487c7152de67e27888b72829a17635749e67ca
roscoevanderboom <roscoevanderboom@protonmail.com>
2024-12-17 17:37:46 +0200

    Update login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
     <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
       <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
-          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>
+          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign in your Tutor-System</h3>
           <p className="text-sm text-gray-500 dark:text-zinc-400">
             Use your email and password to sign in
           </p>
